a58814ee1308b704230e16c0f9a5d5501ababadd
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 19:24:28 -0800

    create the shopping list page

--- src/shopping-list/ShoppingListPage.js
+++ src/shopping-list/ShoppingListPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BackButton } from '../ui';
+import { useShoppingList } from './useShoppingList';
 
 export const ShoppingListPage = () => {
-    return (
-        <div>
-           ShoppingListPage 
-        </div>
-    )
-}
-
-
+  const { isLoading, items } = useShoppingList();
+  return (
+    <div className="page">
+      <BackButton />
+      <div className="centered-container">
+        <h1>Here's your shopping list: </h1>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          items.map((item) => <p key={item}>{item}</p>)
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/shopping-list/useShoppingList.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export const useShoppingList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const loadShoppingList = async () => {
+      const response = await fetch('/shopping-list');
+      const items = await response.json();
+      setItems(items);
+      setIsLoading(false);
+    };
+    loadShoppingList();
+  }, []);
+  
+  return { isLoading, items };
+};
