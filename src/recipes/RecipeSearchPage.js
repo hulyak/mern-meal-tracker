97c1301c31976c6c1d57a1fb59681d870e2e044b
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 17:11:45 -0800

    create the recipe search and custom hook

--- src/recipes/RecipeSearchPage.js
+++ src/recipes/RecipeSearchPage.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useIngredients } from '../ingredients';
+import { BackButton } from '../ui';
+import { useRecipeSearchResults } from './useRecipeSearchResults';
+
 
 export const RecipeSearchPage = () => {
-    return (
-        <div>
-            RecipeSearchPage
-        </div>
-    )
-}
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const [searchString, setSearchString] = useState('');
+  const { ingredients } = useIngredients();
+  const { searchResults } = useRecipeSearchResults(searchString);
 
+  console.log(searchResults);
+
+  const onSearchClicked = () => {
+    setSearchString(searchInputValue);
+  };
+
+  return (
+    <div className="page">
+      <BackButton />
+      <div className="centered-container">
+        <h1>Add Meal to Plan</h1>
+        <input
+          type="text"
+          className="full-width space-before space-after"
+          placeholder="enter keyword here"
+          value={searchInputValue}
+          onChange={(e) => setSearchInputValue(e.target.value)}
+        />
+        <button className="full-width space-after" onClick={onSearchClicked}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
